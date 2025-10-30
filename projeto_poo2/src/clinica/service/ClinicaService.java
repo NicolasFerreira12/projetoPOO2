4517a06c9eadf4c238dbbd34343cf32e392e03e5
unknown <nicolasferfute@gmail.com>
2025-10-30 20:32:15 -0300

    comment ClinicaService

--- projeto_poo2/src/clinica/service/ClinicaService.java
+++
@@ -1,4 +0,0 @@
-package clinica.service;
-
-public class ClinicaService {
-}
--- projeto_poo2/src/restaurante/service/RestauranteService.java
+++ projeto_poo2/src/restaurante/service/RestauranteService.java
@@ -1,4 +1,53 @@
 package restaurante.service;
 
-public class RestauranteService {
+import restaurante.model.*;
+import java.util.*;
+
+public class RestauranteService
+{
+    private List<Voucher> vouchers = new ArrayList<>();
+    private List<Pagamento> pagamentos = new ArrayList<>();
+
+    public void addVoucher(Voucher voucher)
+    {
+        vouchers.add(voucher);
+    }
+
+    public void addPagamento(Pagamento pagamento)
+    {
+        pagamentos.add(pagamento);
+    }
+
+    // Métodos para integração
+
+    public List<Voucher> getVouchers()
+    {
+        return new ArrayList<>(vouchers);
+    }
+
+    public boolean utilizouRestaurante(String pessoaId)
+    {
+        for (Pagamento pagamento : pagamentos)
+        {
+            if (pagamento.getPessoaId().equals(pessoaId) && pagamento.isConfirmado())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Método simplificado para exemplo
+    public double getGastoTotal(String pacienteId)
+    {
+        double total = 0;
+        for (Pagamento pagamento : pagamentos)
+        {
+            if (pagamento.getPessoaId().equals(pacienteId))
+            {
+                total += pagamento.getValor();
+            }
+        }
+        return total;
+    }
 }
