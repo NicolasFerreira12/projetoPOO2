4b7ddbf08afdfb39b836a600e4d9dbe0ac6946f7
Leonardo D'Amorim Cireno Gomes <150437548+Zarabat0@users.noreply.github.com>
2025-10-30 20:21:55 -0300

    Implement patient management in ClinicaService
    Added methods to manage patients in ClinicaService.

--- projeto_poo2/src/clinica/service/ClinicaService.java
+++ projeto_poo2/src/clinica/service/ClinicaService.java
@@ -1,4 +1,45 @@
 package clinica.service;
 
-public class ClinicaService {
+import clinica.model.*;
+import java.util.*;
+
+public class ClinicaService
+{
+    private List<Paciente> pacientes = new ArrayList<>();
+    private List<Medico> medicos = new ArrayList<>();
+
+    public void addPaciente(Paciente paciente)
+    {
+        pacientes.add(paciente);
+    }
+
+    public List<Paciente> getPacientes() 
+    {
+        return new ArrayList<>(pacientes);
+    }
+
+    public boolean isPaciente(String pessoaId) 
+    {
+        for (Paciente p : pacientes) 
+        {
+            if (p.getId().equals(pessoaId)) 
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Paciente> getPacientesComDesconto() 
+    {
+        List<Paciente> comDesconto = new ArrayList<>();
+        for (Paciente p : pacientes) 
+        {
+            if (p.isTemDesconto()) 
+            {
+                comDesconto.add(p);
+            }
+        }
+        return comDesconto;
+    }
 }
